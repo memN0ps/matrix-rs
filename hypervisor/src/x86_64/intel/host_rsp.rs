102d4d895cde0f6e619300e7f76974f40f6131a1
memN0ps <89628341+memN0ps@users.noreply.github.com>
2023-10-16 16:38:18 +1300

    Updated host.rsp and added self_data and populated struct

--- hypervisor/src/x86_64/intel/host_rsp.rs
+++ hypervisor/src/x86_64/intel/host_rsp.rs
@@ -1,27 +1,36 @@
-use {alloc::boxed::Box, kernel_alloc::KernelAlloc};
-
 use crate::{error::HypervisorError, println};
+use {alloc::boxed::Box, core::mem::size_of, kernel_alloc::KernelAlloc};
 
 pub const KERNEL_STACK_SIZE: usize = 0x6000;
-pub const STACK_CONTENTS_SIZE: usize = KERNEL_STACK_SIZE - (core::mem::size_of::<*mut u64>() * 2); // 0x6000 - 16 bytes for alignment/padding
+pub const HOST_RSP_RESERVED: usize = size_of::<*mut u64>() * 3;
+pub const STACK_CONTENTS_SIZE: usize = KERNEL_STACK_SIZE - HOST_RSP_RESERVED;
 
 #[repr(C, align(4096))]
 pub struct HostRsp {
     pub stack_contents: [u8; STACK_CONTENTS_SIZE],
+
+    /// Provides a reference to the `Vmx` structure which can be utilized within the VM exit handlers.
+    /// By leveraging an offset of `[RSP + 8]`, we can retrieve this "self data" reference during the handler's execution, if required.
+    pub self_data: *mut u64,
+
     // To keep Host Rsp 16 bytes aligned
     pub padding_1: u64,
     pub reserved_1: u64,
 }
-const_assert_eq!(core::mem::size_of::<HostRsp>(), KERNEL_STACK_SIZE);
-const_assert_eq!(STACK_CONTENTS_SIZE % 16, 0);
+const_assert_eq!(size_of::<HostRsp>(), KERNEL_STACK_SIZE);
+const_assert_eq!(size_of::<HostRsp>() % 16, 0);
 
 impl HostRsp {
     pub fn new() -> Result<Box<Self, KernelAlloc>, HypervisorError> {
         println!("Setting up VMCS_HOST_RSP region");
-        let host_rsp: Box<HostRsp, KernelAlloc> =
+        let mut host_rsp: Box<HostRsp, KernelAlloc> =
             unsafe { Box::try_new_zeroed_in(KernelAlloc)?.assume_init() };
 
         println!("VMCS_HOST_RSP Virtual Address: {:p}", host_rsp);
+
+        host_rsp.stack_contents = [0u8; STACK_CONTENTS_SIZE];
+        host_rsp.reserved_1 = u64::MAX;
+        host_rsp.padding_1 = u64::MAX;
 
         println!("VMCS_HOST_RSP successful!");
 
