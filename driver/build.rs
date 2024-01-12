090a9d9191771f98ceceefefe02a3398e2ac5830
memN0ps <89628341+memN0ps@users.noreply.github.com>
2024-01-13 11:05:48 +1300

    The hypervisor loads on Windows 10 now instead of just 11.
    Thanks Matthias again :)
    
    https://github.com/microsoft/windows-drivers-rs/issues/42

--- driver/build.rs
+++ driver/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), wdk_build::ConfigError> {
-    wdk_build::Config::from_env_auto()?.configure_binary_build();
+    let mut config = wdk_build::Config::from_env_auto()?;
+    config.driver_config = wdk_build::DriverConfig::WDM();
+    config.configure_binary_build();
     Ok(())
 }
